75d0dacbcbe9c748a2b840f521810868029246e4
Julianertle <julian_ertle@web.de>
2024-11-10 15:51:44 +0100

    added preview for jetpack compose

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -4,6 +4,8 @@ import WeatherApp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.jetpackcompose.data.WeatherRepositoryImpl
 import com.example.jetpackcompose.domain.WeatherViewModel
@@ -21,4 +23,11 @@ class MainActivity : ComponentActivity() {
             WeatherApp(viewModel)
         }
     }
+}
+
+@Preview(showBackground = true, name = "Weather App Preview")@Composable
+fun PreviewWeatherApp() {
+    // Create a mock or default WeatherViewModel for preview
+    val viewModel = WeatherViewModel(WeatherRepositoryImpl())
+    WeatherApp(viewModel)
 }
