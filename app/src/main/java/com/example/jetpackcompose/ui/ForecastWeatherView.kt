1cca25872acd20744f3b333919df13d3b2ca124e
Julianertle <julian_ertle@web.de>
2024-12-16 17:57:06 +0100

    forecast added searchbar

--- app/src/main/java/com/example/jetpackcompose/ui/ForecastWeatherView.kt
+++ app/src/main/java/com/example/jetpackcompose/ui/ForecastWeatherView.kt
@@ -1,5 +1,6 @@
 package com.example.jetpackcompose.ui
 
+import SearchBarSample
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.*
@@ -15,12 +16,17 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.jetpackcompose.model.WeatherViewModel
 import kotlinx.coroutines.flow.map
 
 @Composable
 fun ForecastWeatherView(forecast: List<ForecastItem>) {
     val context = LocalContext.current
     var hometown by remember { mutableStateOf("") }
+    var apiKey by remember { mutableStateOf("") }
+    val weatherViewModel: WeatherViewModel = viewModel() // This creates the view model instance
+
 
     // Retrieve hometown from DataStore
     LaunchedEffect(Unit) {
@@ -31,7 +37,40 @@ fun ForecastWeatherView(forecast: List<ForecastItem>) {
         }
     }
 
+    // Retrieve hometown and apiKey from DataStore when the view is opened
+    LaunchedEffect(Unit) {
+        // Collect both hometown and apiKey
+        context.dataStore.data.collect { preferences ->
+            // Retrieve values from DataStore
+            hometown = preferences[Keys.HOMETOWN_KEY] ?: ""
+            apiKey = preferences[Keys.API_TOKEN_KEY] ?: ""  // Ensure you have a key in the DataStore for API_KEY
+
+            // Automatically fetch weather for hometown if it exists
+            if (hometown.isNotEmpty()) {
+                weatherViewModel.fetchWeatherData(hometown, apiKey)
+            }
+        }
+    }
+
     val searchQuery = rememberSaveable { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+    ) {
+        // Pass hometown as the initial query to the search bar
+        SearchBarSample(
+            selectedMenu = "Forecast",
+            apiKey = apiKey,
+            onQueryChanged = { query ->
+                searchQuery.value = query // Update the search query when the input changes
+                if (query.isNotEmpty()) {
+                    weatherViewModel.fetchWeatherData(query,apiKey)
+                }
+            }
+        )
+    }
 
     Column(
         modifier = Modifier
